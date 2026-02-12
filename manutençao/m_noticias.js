135f1651b9d120e1669099ede580fa1b8ea6fe25
igudevkit <kaneki22igor@gmail.com>
2026-02-12 15:37:06 -0300

    adicionado parte dos assets oficiais ao site

--- manutençao/m_noticias.js
+++ manutençao/m_noticias.js
@@ -9,20 +9,11 @@ Este arquivo é consumido por todas as páginas públicas do site.
 const itens = [
   {
     id: 1,
-    titulo: "Mar de Rendeiras",
-    descricao: "Obra inspirada na cultura das rendeiras e na vida costeira.",
+    titulo: "Nada Aqui :(",
+    descricao: "Nada Aqui :(",
     materia:
-      "hoje às 10:00 finalizei esta pintura que retrata as rendeiras. Espero que gostem! ela está disponível para venda.",
+      "Nada Aqui :(",
     data: "03/09/2023",
-    imagem: "/assets/95672760_p0.png",
-  },
-  {
-    id: 2,
-    titulo: "Mar de Rendeiras",
-    descricao: "Obra inspirada na cultura das rendeiras e na vida costeira.",
-    materia:
-      "hoje às 10:00 finalizei esta pintura que retrata as rendeiras. Espero que gostem! ela está disponível para venda.",
-    data: "03/09/2013",
-    imagem: "/assets/95995158_p0.png",
+    imagem: "",
   },
 ];
